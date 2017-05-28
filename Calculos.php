2ea83edc6e655ca40b6279b629519cd180e0dbe4
Amaury <reimy316@hotmail.com>
2017-05-29 00:29:50 +0200

    Añadida la versión calcular peso ideal

--- Calculos.php
+++ Calculos.php
@@ -4,5 +4,52 @@
        static public function calcularImc($peso,$altura){
             return ($peso/(pow($altura,2)));
         }
+
+        static public function resultadoImc($imc){
+            $resultado;
+            if($imc<16){
+                $resultado="Delgadez severa";
+            }
+            elseif($imc>=16 && $imc<17){
+                $resultado="Delgadez moderada";
+            }
+            elseif($imc>=17 && $imc<18.50){
+                $resultado="Delgadez leve";
+            }
+            elseif($imc>=18.50 && $imc<25){
+                $resultado="Peso normal";
+            }
+            elseif($imc>=25 && $imc<30){
+                $resultado="Sobrepeso";
+            }
+            elseif($imc>=30 && $imc<35){
+                $resultado="Obesidad: Tipo I";
+            }
+            elseif($imc>=35 && $imc<40){
+                $resultado="Obesidad: Tipo II";
+            }
+            elseif($imc>45){
+                $resultado="Obesidad: Tipo III";
+            }
+            
+            return $resultado;
+        }
+        //Devuelve el peso ideal, en caso de no especificarse el sexo se devuelve cero.
+        static public function pesoIdeal($peso,$altura,$sexo){
+            $pesoIdeal=0;
+            $sexo=strtolower($sexo);
+            $altura=$altura*100;
+            if($sexo=="hombre"){
+                $pesoIdeal=((($altura-150)*0.75)+50);
+            }
+            elseif($sexo=="mujer"){
+                $pesoIdeal=((($altura-150)*0.6)+50);
+            }
+            else{
+                $pesoIdeal=0;
+            }
+
+            return $pesoIdeal;
+        }
     }
 ?>
--- Modelo.php
+++ Modelo.php
@@ -2,7 +2,9 @@
     require_once "Calculos.php";
     require_once "Usuario.php";
 
-    $user = new Usuario ("Amaury","Hombre","124587","reimy316@hotmail.com",85,1.73);
+    $user = new Usuario ("Evilla","Mujer","124587","reimy316@hotmail.com",45,1.56);
 
-    echo round(Calculos::calcularImc($user->getPeso(),$user->getAltura()));
+    //echo round((Calculos::calcularImc($user->getPeso(),$user->getAltura())),2);
+    //echo Calculos::resultadoImc(Calculos::calcularImc($user->getPeso(),$user->getAltura()));
+    echo Calculos::pesoIdeal($user->getPeso(),$user->getAltura(),$user->getSexo());
 ?>
