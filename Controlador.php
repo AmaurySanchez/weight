96987a319686101e5067031f7371dbc97c1ed8f2
Amaury <reimy316@hotmail.com>
2017-06-05 23:02:19 +0200

    Haciendo calorias diarias

--- Controlador.php
+++ Controlador.php
@@ -2,11 +2,13 @@
 //require_once "Modelo.php";
 require_once "Calculos.php";
 
-if(isset($_POST["peso"]) && isset($_POST["altura"])){
-     echo round((Calculos::calcularImc($_POST["peso"],$_POST["altura"])),2);
+if((isset($_POST["peso"]) && isset($_POST["altura"])) && !isset($_POST["sexo"])){
+    $imc=round((Calculos::calcularImc($_POST["peso"],$_POST["altura"])),2);
+    $estado=Calculos::resultadoImc($imc);
+     echo $imc." : ".$estado;
 }
-else{
-    echo "error";
+elseif(isset($_POST["peso"]) && isset($_POST["altura"]) && isset($_POST["sexo"]) && isset($_POST["edad"]) && isset($_POST["nivelActividadFisica"])){
+    echo "Holaaa";
 }
 
 ?>
